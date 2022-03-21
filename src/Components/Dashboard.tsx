9f6b5dd2dac35c47948ffa1ae38d77b53d92bd35
shidomnt <hatranquang02@gmail.com>
2022-03-21 18:57:02 +0700

    up deploy

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -13,7 +13,6 @@ import {
 import React, { useEffect, useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { searchByQuery } from '../api';
-import { ReactComponent as Logo } from '../assets/img/logo.svg';
 import Breadcrumb from './Breadcrumb';
 import { Truyen } from './types';
 
